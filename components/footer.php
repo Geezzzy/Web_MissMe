a22f6460e23b87c02d40e5d8227c2e2fb308c19c
Geezzzy <gladysohyver01@gmail.com>
2024-06-28 12:54:42 +0700

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,45 @@
+    <footer>
+        <div class="content">
+            <div class="box">
+                <img src="image/logo2.png" width="190px" alt="">
+                <p>We’re always in search for talented and motivated people. Don’t be shy introduce yourself!</p>
+                <a href="contact.php" class="btn">contact with us</a>
+            </div>
+            <div class="box">
+                <!-- <h3>my account</h3>
+                <a href=""><i class='bx bx-chevron-right'></i>My Account</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Order History</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Wish List</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Newsletter</a> -->
+            </div>
+            <div class="box">
+                <!-- <h3>Information</h3>
+                <a href="contact.php"><i class='bx bx-chevron-right'></i>About Us</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Privacy Policy</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Terms & Conditions</a> -->
+            </div>
+            <!-- <div class="box">
+                <h3>extras</h3>
+                <a href="contact.php"><i class='bx bx-chevron-right'></i>Brands</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Gift Certificates</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Affiliate</a>
+                <a href=""><i class='bx bx-chevron-right'></i>Specials</a>
+            </div> -->
+            <div class="box">
+                <h3>contact us</h3>
+                <p><i class='bx bxs-phone'></i> +6281393359919</p>
+                <p><i class='bx bxs-envelope' ></i>missMe@gmail.com</p>
+                <p><i class='bx bxs-location-plus' ></i>Tanjung Duren Utara, Jakarta Barat</p>
+                <div class="icons">
+                    <i class='bx bxl-facebook'></i>
+                    <i class='bx bxl-twitter'></i>
+                    <i class='bx bxl-instagram' ></i>
+                    <i class='bx bxl-linkedin'></i>
+                </div>
+            </div>
+        </div>
+        <div class="bottom">
+            <p>Copyright ©2024 MissMe Ice Cream. All Rights Reserved</p>
+            <a href="adminpannel/login.php">Admin Dashboard </a>
+        </div>
+    </footer>
